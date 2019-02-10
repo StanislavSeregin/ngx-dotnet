63a84b73bbc6f7d05285b9f22c983c53f8700d3f
stas seregin <stas_seregin@mail.ru>
2019-02-10 18:55:03 +0300

    Attach DotnetDemo.csproj to project.

---
+++ DotnetDemo/DotnetDemo/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DotnetDemo
+{
+    public class Class1
+    {
+        private static int _counter;
+
+        static Class1()
+        {
+            _counter = 0;
+        }
+
+        public static int CounterNext()
+        {
+            _counter++;
+            return _counter;
+        }
+
+        public static string GetNewGuid()
+        {
+            var guid = Guid.NewGuid();
+            return guid.ToString();
+        }
+    }
+}
